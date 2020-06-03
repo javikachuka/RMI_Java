615f2662580ed47fcc4066e550b3cc0295d10f71
facundoSilveira <facundosilveiradosanto@gmail.com>
2020-06-03 11:16:01 -0300

    Update Almacenamiento.java

--- Servidor_API/src/modelos/Almacenamiento.java
+++ Servidor_API/src/modelos/Almacenamiento.java
@@ -8,12 +8,39 @@ package modelos;
 import java.rmi.server.UnicastRemoteObject;
 import interfaces.AlmacenamientoInterface;
 import java.rmi.RemoteException;
+import java.rmi.server.RMIClientSocketFactory;
+import java.rmi.server.RMIServerSocketFactory;
+import java.util.ArrayList;
+import java.util.Hashtable;
+
 /**
  *
  * @author kachu
  */
 public class Almacenamiento extends UnicastRemoteObject implements AlmacenamientoInterface{
 
+   // private ArrayList<ArrayList> almacenamiento; 
+    Hashtable<String, String> almacenamiento = new Hashtable<String, String>();
+
+    public Almacenamiento(ArrayList<ArrayList> almacenamiento) throws RemoteException {
+        this.almacenamiento = almacenamiento;
+    }
+
+    public Almacenamiento(ArrayList<ArrayList> almacenamiento, int port) throws RemoteException {
+        super(port);
+        this.almacenamiento = almacenamiento;
+    }
+
+    public Almacenamiento(ArrayList<ArrayList> almacenamiento, int port, RMIClientSocketFactory csf, RMIServerSocketFactory ssf) throws RemoteException {
+        super(port, csf, ssf);
+        this.almacenamiento = almacenamiento;
+    }
+
+    public ArrayList<ArrayList> getAlmacenamiento() {
+        return almacenamiento;
+    }
+    
+    
     @Override
     public int guardar(String clave, String valor) throws RemoteException {
         
